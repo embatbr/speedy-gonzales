b986b5178c1034d83dc580600b3fd3452c78d8af
Eduardo Tenório <eduardo.tenorio@creditas.com.br>
2018-11-09 19:32:50 -0200

    Added file spark/spark.pid to .gitignore and created script to stop Spark.

---
+++ spark/stop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+export SUBROOT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd $SUBROOT_PATH
+
+
+DIR2DELETE="$1"
+
+
+kill -9 $(cat spark.pid)
+rm spark/spark.pid
+rm -Rf "$DIR2DELETE"

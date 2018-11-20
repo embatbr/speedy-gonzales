50babc295230ecc4b923c8b33ed03853fa4053fc
Eduardo Tenório <embatbr@gmail.com>
2018-11-19 22:44:42 -0200

    More utilities for function format_tables.

--- spark/functions/functions_utils.py
+++ spark/functions/functions_utils.py
@@ -2,24 +2,77 @@
 
 import boto3
 import csv
+from datetime import datetime as dt
 from io import StringIO
 
 
 FORMATTERS = {
-    'trim': lambda s: s.strip(),
+    'trim': lambda s: _trim(s),
     'pretty_cpf': lambda cpf: _pretty_cpf(cpf),
-    'pretty_pis': lambda pis: _pretty_pis(pis)
+    'pretty_pis': lambda pis: _pretty_pis(pis),
+    'to_date': lambda date: _to_date(date),
+    'timestamp_to_date': lambda timestamp: _timestamp_to_date(timestamp),
+    'ensure_int': lambda int_candidate: _ensure_int(int_candidate)
 }
 
+def _trim(s):
+    if s is None:
+        return None
+
+    trimmed = s.strip()
+    if not trimmed:
+        return None
+
+    return trimmed
+
 def _pretty_cpf(cpf):
-    if cpf is None:
-        return cpf
-    return '{}.{}.{}-{}'.format(cpf[:3], cpf[3:6], cpf[6:9], cpf[9:])
+    trimmed = _trim(cpf)
+    if not trimmed:
+        return None
+
+    return '{}.{}.{}-{}'.format(trimmed[:3], trimmed[3:6], trimmed[6:9], trimmed[9:])
 
 def _pretty_pis(pis):
-    if pis is None:
-        return pis
-    return '{}.{}.{}-{}'.format(pis[:3], pis[3:8], pis[8:10], pis[10])
+    trimmed = _trim(pis)
+    if not trimmed:
+        return None
+
+    return '{}.{}.{}-{}'.format(trimmed[:3], trimmed[3:8], trimmed[8:10], trimmed[10])
+
+def _to_date(date_str):
+    trimmed = _trim(date_str)
+    if not trimmed:
+        return None
+
+    try:
+        date = dt.strptime(trimmed, '%Y-%m-%d')
+        return date.strftime('%Y-%m-%d')
+    except Exception:
+        return None
+
+def _timestamp_to_date(timestamp):
+    trimmed = _trim(timestamp)
+    if not trimmed:
+        return None
+
+    try:
+        trimmed = trimmed.split('T')[0]
+        date = dt.strptime(trimmed, '%Y-%m-%d')
+        return date.strftime('%Y-%m-%d')
+    except Exception:
+        return None
+
+def _ensure_int(int_candidate):
+    if isinstance(int_candidate, int):
+        return int_candidate
+
+    if not isinstance(int_candidate, str):
+        return None
+
+    if not int_candidate.isdigit():
+        return None
+
+    return int(int_candidate)
 
 
 def transform(memory, func, err_class, filter_by_success=True):
--- spark/stop.sh
+++ spark/stop.sh
@@ -5,9 +5,9 @@ export SUBROOT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $SUBROOT_PATH
 
 
-DIR2DELETE="$1"
+# DIR2DELETE="$1"
 
 
 kill -9 $(cat spark.pid)
 rm spark.pid
-rm -Rf "$DIR2DELETE"
+# rm -Rf "$DIR2DELETE"
